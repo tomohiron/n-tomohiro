82ba5c75a3addca635599aa49627fce3b8466e10
nakamura.tomohiro <nakamura.tomohiro@1a61a1e0-3e11-11de-90e1-f9f1724fc9cc>
2010-03-14 15:13:48 +0000

    管理画面メッセージ入力欄拡張、link機能追加

--- trunk/gacha/src/jp/gacha/AdminService.java
+++ trunk/gacha/src/jp/gacha/AdminService.java
@@ -28,4 +28,25 @@ public class AdminService {
         return messages.get(0).getValue();
     }
 
+    public static void updateLink(String value) {
+        Link link = new Link(value);
+        PersistenceManager pm = PMF.get().getPersistenceManager();
+        try {
+            pm.makePersistent(link);
+        } finally {
+            pm.close();
+        }
+    }
+
+    public static String getLink() {
+        String query = "select from " + Link.class.getName() + " order by createdAt desc";
+        PersistenceManager pm = PMF.get().getPersistenceManager();
+        List<Link> links = (List<Link>) pm.newQuery(query).execute();
+
+        if (links.isEmpty()) {
+            return "";
+        }
+
+        return links.get(0).getValue();
+    }
 }
